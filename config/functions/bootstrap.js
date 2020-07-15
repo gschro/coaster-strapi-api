17fd84dbb3812c180d543f7b7cdda9de7e34c3ba
Garrett Schroath <gtschroath@gmail.com>
2020-07-15 07:24:43 -0500

    fixed db

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,8 +10,26 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
 
- if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+ const parseDatbaseUrl = url => {
+   const pattern = /[a-z]+:\W\W([a-z]+):([a-z0-9]+)@([a-z0-9-.]+):([0-9]+)\W([a-z0-9]+)/
+   const match = pattern.exec(url);
+   const [, user, password, host, port, database] = match
+
+   process.env.DATABASE_USERNAME = user
+   process.env.DATABASE_PASSWORD = password
+   process.env.DATABASE_HOST = host
+   process.env.DATABASE_PORT = port
+   process.env.DATABASE_NAME = database
  }
 
-module.exports = () => {};
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+parseDatbaseUrl(process.env.DATABASE_URL)
+
+module.exports = () => {
+
+
+};
+
